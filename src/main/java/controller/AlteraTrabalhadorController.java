7ebe4a9e8021ebab8bf7684dc95eb7a3b4fdbbbd
Kauê Matos <98132837+iKaueMatos@users.noreply.github.com>
2023-05-06 15:09:17 -0300

    Update AlteraTrabalhadorController.java

--- src/main/java/controller/AlteraTrabalhadorController.java
+++ src/main/java/controller/AlteraTrabalhadorController.java
@@ -2,14 +2,12 @@ package controller;
 
 import java.io.IOException;
 import java.net.URLDecoder;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import model.Trabalhador;
 
 @WebServlet("/AlteraTrabalhadorController")
